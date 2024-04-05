0b812456fec2a48b28598833fded15c662171f15
💖 Hana 💖 <gaycoderprincess@proton.me>
2024-04-05 17:02:45 +0200

    fix virtualprotect in hooklib fill

--- nya_commonhooklib.h
+++ nya_commonhooklib.h
@@ -13,9 +13,9 @@ namespace NyaHookLib {
 
 	void Fill(uintptr_t address, uint8_t value, unsigned int count) {
 		DWORD backup, backup2;
-		VirtualProtect((void*)address, sizeof(value), PAGE_EXECUTE_READWRITE, &backup);
+		VirtualProtect((void*)address, count, PAGE_EXECUTE_READWRITE, &backup);
 		memset((void*)address, value, count);
-		VirtualProtect((void*)address, sizeof(value), backup, &backup2);
+		VirtualProtect((void*)address, count, backup, &backup2);
 	}
 
 	inline intptr_t MakeRelative(uintptr_t src, uintptr_t dest) {
